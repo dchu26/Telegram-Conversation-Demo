34a5d53e5cb7e98f1b399f61723e670218c84e2a
dchu26 <dchu26@ucmerced.edu>
2023-06-18 13:59:45 -0700

    Update to parse Google Sheet for previous entries and update database accordingly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import re
-import csv
 import json
+import csv
+import ast
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from collections import defaultdict
@@ -9,25 +10,21 @@ from telethon import events
 from telethon import TelegramClient
 from telethon.tl.functions.users import GetFullUserRequest
 from google.cloud import secretmanager
-#from dotenv import load_dotenv, find_dotenv
 
 # Scope for the Google service account
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
-# Add here
+# ADD BELOW HERE #
+
+
+##################
 
 # Credentials from Google cloud account
 credentials = ServiceAccountCredentials.from_json_keyfile_dict(client_json, scope)
 gclient = gspread.authorize(credentials)
 
-# Loads the .env file
-#load_dotenv(find_dotenv())
-
-# Retrieves the API_ID and API_HASH from the .env file
-#api_id = os.environ.get("API_ID")
-#api_hash = os.environ.get("API_HASH")
-
+# Credentials for user
 api_id = anon_json["API_ID"]
 api_hash = anon_json["API_HASH"]
 
@@ -36,21 +33,34 @@ api_hash = anon_json["API_HASH"]
 client = TelegramClient('anon', api_id, api_hash)
 
 # Names of the people whom you've had a conversation with
-database = defaultdict(dict)
 dialog_names = []
 
+# Database for our entries (key = Telegram user id, value = all other attributes)
+database = defaultdict(dict)
+
+# Name of the Google Sheet
+sheet_name = 'CSV-to-Google-Sheet'
+
 # Listener for new messages
-@client.on(events.NewMessage(incoming=True))
+@client.on(events.NewMessage())
 async def handler(event):
+    print(type(database[5748872509]['email']))
+    # Twitter name holder
     t = []
+    # Email holder
     e = []
+    # LinkedIn link holder
     l = []
+    # Person who messaged
     messenger = event.message.peer_id.user_id
+    # Full info of messenger
     fullInfo = await client(GetFullUserRequest(messenger))
+    # Parsing for relevant info
     twitter_finder(event.message.message, t)
     email_finder(event.message.message, e)
     linkedin_finder(event.message.message, l)
 
+    # Situation where we need to make a new entry in our database (new user messages us)
     if messenger not in database:
         database[messenger]["first_name"] = fullInfo.users[0].first_name
         database[messenger]["last_name"] = fullInfo.users[0].last_name
@@ -60,6 +70,7 @@ async def handler(event):
         database[messenger]["email"] = e
         database[messenger]["linkedin"] = l
 
+    # Entry is already in our database
     else:
         database[messenger]["twitter"].extend(t)
         database[messenger]["email"].extend(e)
@@ -71,21 +82,92 @@ async def handler(event):
     # Updates the Google sheet
     await csv_to_google()
 
-#########################################################################
+
+############################################################################
+
+
+# Function that gets information from Google Sheet and populates our database
+async def google_to_dict():
+    # Opening the spreadsheet on the first sheet
+    spreadsheet = gclient.open(sheet_name).sheet1
+    # Getting data from spreadsheet
+    data = spreadsheet.get_all_records()
+    
+    # Getting data for each row
+    for record in data:
+        # Get the first key-value pair
+        first_key = next(iter(record))
+        # Getting the key-value records and storing as a dict, excluding the first key-value pair
+        record_without_first = {key: value for key, value in record.items() if key != first_key}
+        # Initializing database using User ID as the key, and record_without_first as the value
+        database[record[first_key]] = record_without_first
+    
+    # Converting the string representation of the lists into lists when applicable
+    # ast.literal_eval() is quite strict on conversion so we have to check if it is valid to convert before we actually do it
+    for key in database:
+        if is_valid_list_string(database[key]['linkedin']):
+            database[key]['linkedin'] = ast.literal_eval(database[key]['linkedin'])
+        if is_valid_list_string(database[key]['email']):
+            database[key]['email'] = ast.literal_eval(database[key]['email'])
+        if is_valid_list_string(database[key]['twitter']):
+            database[key]['twitter'] = ast.literal_eval(database[key]['twitter'])
+
+
+# Function for moving data from dictionary to CSV file
+async def dic_to_csv():
+    # Write dictionary into csv file
+    
+    # Initializing each column name
+    csv_columns = ['User ID', 'first_name', 'last_name',
+                   'username', 'phone', 'linkedin', 'email', 'twitter']
+    
+    # Try to open the csv file and write to it
+    try:
+        with open("database.csv", "w") as csvfile:
+            # Writing the data to the csv file with these parameters
+            writer = csv.DictWriter(
+                csvfile, fieldnames=csv_columns, lineterminator='\n')
+            # Creating the header in the CSV file
+            writer.writeheader()
+            # Writing everything else
+            for key, val in database.items():
+                # Creating key-value with the User ID key
+                row = {'User ID': key}
+                row.update(val)
+                writer.writerow(row)
+    # Any possible error that occurs with opening the csv file
+    except IOError:
+        print("I/O error")
+
 
 # Basic function to write csv data to the Google sheet
 async def csv_to_google():
-    spreadsheet = gclient.open('CSV-to-Google-Sheet')
+    # Opens the spreadsheet
+    spreadsheet = gclient.open(sheet_name)
 
+    # Reading and writing to the Google Sheet
     with open('database.csv', 'r') as file_obj:
         content = file_obj.read()
         gclient.import_csv(spreadsheet.id, data=content)
 
+
+# Helper function for checking if a string representation of a list can be converted into a list
+def is_valid_list_string(string_representation):
+    # Situation where it is possible to do so
+    try:
+        ast.literal_eval(string_representation)
+        return True
+    # Situation where the input is invalid for ast.literal_eval()
+    except (SyntaxError, ValueError):
+        print(string_representation)
+        return False
+
 # Function for adding names to dialog_names
 async def name_finder():
     async for dialog in client.iter_dialogs():
-        #print(dialog.name, 'has ID', dialog.id)
+        # print(dialog.name, 'has ID', dialog.id)
         dialog_names.append(dialog.name)
+
 
 # Function for adding emails parsed from a string to emails
 def email_finder(text, emails):
@@ -93,75 +175,33 @@ def email_finder(text, emails):
     match = re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', text)
     emails.extend(match)
 
+
 # Function for adding twitter username parsed from a string to twitter_names
 def twitter_finder(str, twitter_name):
     # Regex expression to match for twitter usernames
     same = re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))(@[A-Za-z0-9-_]+)', str)
     twitter_name.extend(same)
 
+
 # Function for adding linkedin links parsed from a string to linkedin_links
 def linkedin_finder(s, linkedin_links):
     # Regex expression for only linkedin links
     regex = r'(https?:\/\/[\w]+\.?linkedin\.com\/in\/[A-z0-9_-]+\/?)'
-    url = re.findall(regex, s)
-    linkedin_links.extend(url)
+    urls = re.findall(regex, s)
+    linkedin_links.extend(urls)
 
-async def dic_to_csv():
-    # Write dictionary into csv file
-    csv_columns = ['User ID', 'first_name', 'last_name', 'username', 'phone', 'linkedin', 'email', 'twitter']
-    try:
-        with open("database.csv", "w") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-            writer.writeheader()
-            for key, val in sorted(database.items()):
-                row = {'User ID': key}
-                row.update(val)
-                writer.writerow(row)
-    except IOError:
-        print("I/O error")
 
 # Main function
 async def main():
 
-    # Populates dialog_names
-    await name_finder()
-    
-    # Gets the full profile information for the first user in each conversation
-    for name in dialog_names:
-        try:
-            userInfo = await client(GetFullUserRequest(name))
-            user = userInfo.users[0]
-            database[user.id]["first_name"] = user.first_name
-            database[user.id]["last_name"] = user.last_name
-            database[user.id]["username"] = user.username
-            database[user.id]["phone"] = user.phone
-
-            emails = []
-            twitter_name = []
-            linkedin_links = []
-
-            async for message in client.iter_messages(name):
-                email_finder(message.text, emails)
-                twitter_finder(message.text, twitter_name)
-                linkedin_finder(message.text, linkedin_links)
-            
-            database[user.id]["linkedin"] = linkedin_links
-            database[user.id]["email"] = emails
-            database[user.id]["twitter"] = twitter_name
-
-        # Exception when the group chat is named
-        except Exception as e:
-            print("Group Chat called {}".format(name))
-
-    # Where the population of the database into a csv file happens
-    # print(database)
+    # Pulling data from Google Sheets and populating our database (dict)
+    await google_to_dict()
+    # Moving our data from the dict into the CSV
     await dic_to_csv()
-    
-    await csv_to_google()
+
 
 # Runs until main is complete for now, then runs begins event handling
 with client:
-    # Uncomment this line to retrieve full history of previous messages
-    # client.loop.run_until_complete(main())
+    client.loop.run_until_complete(main())
     client.start()
     client.run_until_disconnected()
